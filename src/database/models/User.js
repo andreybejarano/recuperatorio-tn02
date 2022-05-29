faa9a75ec6d96a83e7a85e8d4365b78e8c88bdb0
JuanGyldenfeldt <94145723+GyldenfeldtJuan@users.noreply.github.com>
2022-05-29 12:27:04 -0300

    Create User.js

---
+++ src/database/models/User.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User'
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        password: {
+            type: dataTypes.STRING
+        },
+        roles_id: {
+            type: dataTypes.INTEGER
+        }
+    };
+    let config= {
+        tableName: "users",
+        timestamps: false
+    }
+    const User = sequelize.define(alias, cols, config);
+
+    return User;
+};
