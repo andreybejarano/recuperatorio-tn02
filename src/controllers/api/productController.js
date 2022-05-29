3db784e9672943ca54d6abd92ae88f22f00abe1c
JuanGyldenfeldt <94145723+GyldenfeldtJuan@users.noreply.github.com>
2022-05-29 15:23:39 -0300

    Product View-Controller

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -1,6 +1,33 @@
+const { response } = require('express');
+const db = require('../database/models');
+
 const controller = {
     index: (req, res) => {
         // Crear controladores para retornar todos los productos
+        db.Product.findAll()
+        .then((products) => {
+            res.json(products);
+        }).catch(err => {
+            res.json(err);
+        })
+    },
+    create: (req, res) => {
+        res.render('formProductCreate');
+    },
+    store: (req, res) => {
+        const ProductToCreate = {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            category: req.body.category
+        };
+        db.Product.create(ProductToCreate)
+        .then(response => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.send(err);
+        })
     }
 }
 
--- src/routers/api/product.js
+++ src/routers/api/product.js
@@ -5,6 +5,10 @@ const productController = require('../../controllers/api/productController');
 const router = express.Router();
 
 // Implementar logica para retornar todos los productos
-// router.???('/', productController.index);
+router.get('/', productController.index);
+
+router.get('/formProductCreate', productController.create)
+
+router.post('/', productController.store)
 
 module.exports = router;
