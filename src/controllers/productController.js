b1758f7c353e0c75b4f6cac7bcf3dd51df587f68
JuanGyldenfeldt <94145723+GyldenfeldtJuan@users.noreply.github.com>
2022-05-29 22:50:22 -0300

    Fix Controllers

---
+++ src/controllers/productController.js
@@ -0,0 +1,29 @@
+const { response } = require('express');
+const db = require('../database/models');
+
+const controller = {
+    create: (req, res) => {
+        db.Product.findAll()
+        .then((products) =>{
+            res.render('formProductCreate', {products});
+        })
+        
+    },
+    store: (req, res) => {
+        const ProductToCreate = {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            category: req.body.category
+        };
+        db.Product.create(ProductToCreate)
+        .then(response => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.send(err);
+        })
+    }
+}
+
+module.exports = controller
